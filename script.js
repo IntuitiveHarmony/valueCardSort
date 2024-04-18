5e9b89eb9488a069331d2ba4ef673a4b5598ec37
Intuitive Harmony <j.horst77@gmail.com>
2024-04-18 09:40:35 -0600

    sort cards into arrays

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ import { cards } from "./cardData.js";
 let cardCount = cards.length; // Initially set to the array length but then incremented later
 let currentIndex = 0;
 let shuffledCards;
-let currentCard;
 let notImportantCards = [];
 let somewhatImportantCards = [];
 let importantCards = [];
@@ -17,6 +16,7 @@ let skippedCards = [];
 // DOM Elements
 // ~~~~~~~~~~~~
 // Card Stuff
+const cardElement = $(".card");
 const cardsCountElement = $("#cards-count");
 const cardContainerElement = $(".card-container");
 const cardHeaderElement = $("#card-header");
@@ -53,32 +53,40 @@ const displayCard = () => {
   if (shuffledCards.length > 0) {
     cardHeaderElement.text(shuffledCards[currentIndex].value);
     cardDescriptionElement.text(shuffledCards[currentIndex].description);
+  } else {
+    cardElement.empty();
+    console.log("No more cards");
   }
   updateCardsRemainingDOM();
 };
-const handleEveryImportantButton = () => {
-  currentCard = shuffledCards[currentIndex];
-  console.log("This is the every button function:", currentCard);
-};
-const handleImportantButton = () => {
-  // console.log(importantCards);
-  // console.log(shuffledCards);
-  console.log("handleImportantButton!", currentCard);
-};
-const handleVeryImportantButton = () => {
-  console.log("handleVeryImportantButton!");
-};
-const handleNotImportantButton = () => {
-  console.log("handleNotImportantButton!");
-};
-const handleSomewhatImportantButton = () => {
-  console.log("handleSomewhatImportantButton!");
-};
+function handleEveryImportantButton() {
+  let button = $(this).attr("id"); // get which button was pressed
+  let currentCard = shuffledCards.splice(currentIndex, 1)[0]; // Remove the current card from cards deck
+  // Sort the card based on which button was pressed
+  if (button === "important-button") {
+    importantCards.push(currentCard);
+    console.log("Important button pressed");
+  } else if (button === "very-important-button") {
+    veryImportantCards.push(currentCard);
+    console.log("Very Important button pressed");
+  } else if (button === "somewhat-important-button") {
+    somewhatImportantCards.push(currentCard);
+    console.log("Somewhat Important button pressed");
+  } else if (button === "not-important-button") {
+    notImportantCards.push(currentCard);
+    console.log("Not Important button pressed");
+  }
+  console.log("Shuffled array: ", shuffledCards);
+  console.log("Important array: ", importantCards);
+  console.log("Very Important array: ", veryImportantCards);
+  console.log("Somewhat Important array: ", somewhatImportantCards);
+  console.log("Not Important array: ", notImportantCards);
+  displayCard(); // Update card in the DOM
+}
 const handleSkipButton = () => {
-  let skippedCard = shuffledCards.shift();
-  shuffledCards.push(skippedCard);
+  let skippedCard = shuffledCards.shift(); // Take the card from the front of array
+  shuffledCards.push(skippedCard); // Move card to the back of array
   displayCard();
-  console.log("handleSkipButton!");
 };
 const updateCardsRemainingDOM = () => {
   cardsCountElement.text(`${shuffledCards.length}`);
@@ -86,16 +94,12 @@ const updateCardsRemainingDOM = () => {
 // ~~~~~~~~~~~~~~~~~~~
 // Button Click Events
 // ~~~~~~~~~~~~~~~~~~~
-// Listener for all the important buttons
+// Listener for all the important choice buttons
 allImportantButtonElements.each((index, button) => {
   $(button).click(handleEveryImportantButton);
 });
 
 startButtonElement.click(handleStartButton);
-importantButtonElement.click(handleImportantButton);
-notImportantButtonElement.click(handleNotImportantButton);
-veryImportantButtonElement.click(handleVeryImportantButton);
-somewhatImportantButtonElement.click(handleSomewhatImportantButton);
 skipButtonElement.click(handleSkipButton);
 
 // Do this stuff once the DOM is done loading for the first time
